a65b76d7ca73a54a5b29d65b768f26a583ad0e90
esteban-urrutia-martinez <estebanUrrutiaMartinez@gmail.com>
2025-06-13 16:25:13 -0400

    change deletedAt to DeactivatedAt

--- models/adModel.go
+++ models/adModel.go
@@ -9,13 +9,13 @@ import (
 type Ad struct {
     ID        uint                  `gorm:"primaryKey"`
     
-	Title     string                `gorm:"not null" json:"title" binding:"required"`
+    Title     string                `gorm:"not null" json:"title" binding:"required"`
     ImageUrl  string                `gorm:"not null" json:"image_url" binding:"required,url"`
     Placement string                `gorm:"not null" json:"placement" binding:"required"`
     
-	Active    soft_delete.DeletedAt `gorm:"softDelete:flag,DeletedAtField:DeletedAt;default:1" json:"active"`
+    Active    soft_delete.DeletedAt `gorm:"softDelete:flag,DeletedAtField:DeactivatedAt;default:1" json:"active"`
     
-	CreatedAt time.Time             `json:"created_at"`
+    CreatedAt time.Time             `json:"created_at"`
     UpdatedAt time.Time             `json:"updated_at"`
-    DeletedAt gorm.DeletedAt        `gorm:"index" json:"deleted_at"`
+    DeactivatedAt gorm.DeletedAt    `gorm:"index;column:deactivated_at" json:"deactivated_at"`
 }
